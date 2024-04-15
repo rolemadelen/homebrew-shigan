d8186f455d0a0942a3bc97334dd44ab56e82952c
rolemadelen <rolemadelen17@pm.me>
2024-04-16 06:53:00 +0900

    releasing v0.1.0

--- Formula/shigan.rb
+++ Formula/shigan.rb
@@ -5,7 +5,7 @@ class Shigan < Formula
     desc "Command-line Time Tracker written in Rust"
     homepage "https://github.com/rolemadelen/shigan"
     url "https://github.com/rolemadelen/shigan/releases/download/v0.1.0/shigan-mac.tar.gz"
-    sha256 "b21e7f430851b05730b0775f8d9bb98bfb4220e65c9e1b86b62ec755005fe067"
+    sha256 "6b6cc01f87a12927e5774bf10dfafe170bf4e9e6615c3867607da4dc55a85f67"
     version "0.1.0"
   
     def install
