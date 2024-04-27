5d7f677d85487346e6e7bf6631a563d0ea58b389
rolemadelen <rolemadelen17@pm.me>
2024-04-27 04:09:48 -0400

    Update shigan.rb

--- Formula/shigan.rb
+++ Formula/shigan.rb
@@ -3,8 +3,8 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Shigan < Formula
     desc "Command-line Time Tracker written in Rust"
-    homepage "https://github.com/rolemadelen/shigan"
-    url "https://github.com/rolemadelen/shigan/releases/download/v0.1.0/shigan-mac.tar.gz"
+    homepage "https://github.com/img9417/shigan"
+    url "https://github.com/img9417/shigan/releases/download/v0.1.0/shigan-mac.tar.gz"
     sha256 "6b6cc01f87a12927e5774bf10dfafe170bf4e9e6615c3867607da4dc55a85f67"
     version "0.1.0"
   
